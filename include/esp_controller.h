19d632faaad47ec67ceceafdbd1a01bac6548645
Maksim Dalgeda <maksim.dalgeda@gmail.com>
2026-09-17 18:13:20 +0300

    add esp_controller header

---
+++ include/esp_controller.h
@@ -0,0 +1,19 @@
+#ifndef ESP_CONTROLLER_H
+#define ESP_CONTROLLER_H
+
+#include "device_manager.h"
+#include "error.h"
+
+ESP_Error esp_controller_connect(Device *device);
+
+ESP_Error esp_controller_disconnect(Device *device);
+
+ESP_Error esp_controller_test_connection(Device *device);
+
+ESP_Error esp_controller_on(Device *device, int pin);
+
+ESP_Error esp_controller_off(Device *device, int pin);
+
+ESP_Error esp_controller_get(Device *device, int pin, const char *model, const char *sensor);
+
+#endif
