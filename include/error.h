6628d333f17a79122b7823ca49841613f542366f
Maksim Dalgeda <maksim.dalgeda@gmail.com>
2026-09-17 16:10:12 +0300

    add error enum parsing to string

---
+++ include/error.h
@@ -0,0 +1,14 @@
+#ifndef ERROR_H
+#define ERROR_H
+
+typedef enum
+{
+    OK,
+    ERROR,
+    ERR_UBUS_CONNECT,
+    ERR_UBUS_INVOKE,
+    ERR_UBUS_NOT_INITIALIZED
+
+} ;
+
+#endif
---
+++ src/error.c
@@ -0,0 +1,23 @@
+#include "error.h"
+
+const char *error_to_string(Error err)
+{
+    switch (err)
+    {
+        case OK:
+            return "OK";
+
+        case ERROR:
+            return "ERROR";
+
+        case ERR_UBUS_CONNECT:
+            return "ERR_UBUS_CONNECT";
+
+        case ERR_UBUS_INVOKE:
+            return "ERR_UBUS_INVOKE";
+
+        case ERR_UBUS_NOT_INITIALIZED:
+            return "ERR_UBUS_NOT_INITIALIZED";
+        
+    }
+}
