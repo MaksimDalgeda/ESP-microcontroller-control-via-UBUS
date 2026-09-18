72465c2b752d40e8864ad15920fc9b216b24ec15
Maksim Dalgeda <maksim.dalgeda@gmail.com>
2026-09-18 13:58:49 +0300

    add signal handler

---
+++ include/signal_handler.h
@@ -0,0 +1,10 @@
+#ifndef SIGNAL_HANDLER_H
+#define SIGNAL_HANDLER_H
+#include <signal.h>
+
+extern volatile sig_atomic_t stop;
+
+void sigint_handler(int signal);
+void set_signal_action(void);
+
+#endif
