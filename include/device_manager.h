50de75232ba3c5022a907506df860df250d3c0e8
Maksim Dalgeda <maksim.dalgeda@gmail.com>
2026-09-18 14:00:44 +0300

    update devuce_manager module

--- include/device_manager.h
+++ include/device_manager.h
@@ -12,8 +12,14 @@ typedef struct
     int pid;
 } Device;
 
+ESP_Error device_manager_init(void);
+
 ESP_Error device_manager_find_devices(Device **devices, uint32_t *count);
+ESP_Error device_manager_update_devices(void);
+ESP_Error device_manager_wait_for_change(void); 
+ESP_Error device_manager_get_devices(Device **devices, uint32_t *count);
 
 void device_manager_free_devices(Device *devices, uint32_t count);
+ESP_Error device_manager_clear_data(void);
 
 #endif
--- src/device_manager.c
+++ src/device_manager.c
@@ -1,32 +1,191 @@
 #include "device_manager.h"
 
-#include <libserialport.h>
+#include <syslog.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/inotify.h>
+#include <libserialport.h>
+
+static int inotify_fd = -1;
+static int inotify_watch = -1;
+
+ESP_Error device_manager_init(void)
+{
+    inotify_fd = inotify_init1(0);
+
+    if (inotify_fd == -1)
+        return ERROR;
+
+    inotify_watch = inotify_add_watch(
+        inotify_fd,
+        "/dev",
+        IN_CREATE |
+        IN_DELETE |
+        IN_MOVED_TO |
+        IN_MOVED_FROM
+    );
+
+    if (inotify_watch == -1){
+        close(inotify_fd);
+        inotify_fd = -1;
+
+        return ERROR;
+    }
+
+    return OK;
+}
 
 ESP_Error device_manager_find_devices(Device **devices, uint32_t *count)
 {
     struct sp_port **ports = NULL;
+    enum sp_return result;
+    
+    if(devices == NULL || count == NULL){
+        syslog(LOG_ERR, "Variable(s) not initialized in device_manager");
+        return ERR_NULL_POINTER;
+    }
+    
+    *devices = NULL;
+    *count = 0;
 
-    enum sp_return result = sp_list_ports(&ports);
+    result = sp_list_ports(&ports);
 
-    if (result != SP_OK)
-    {
-        return ERROR;
+    if (result != SP_OK){
+        syslog(LOG_ERR, "Unable to get available serial ports");
+        return ERR_GET_PORT_LIST;
     }
 
-    for (int i = 0; ports[i] != NULL; i++)
-    {
+    uint32_t device_count = 0;
+
+
+    for (uint32_t i = 0; ports[i] != NULL; i++){
+        int vid;
+        int pid;
+
+        result = sp_get_port_usb_vid_pid(ports[i], &vid, &pid);
+
+        if (result != SP_OK)
+            continue;
+
+        device_count++;
+    }
+
+     if (device_count == 0){
+        sp_free_port_list(ports);
+        return OK;
+    }
+    
+    Device *device_list = calloc(device_count, sizeof(Device));
+
+    if (device_list == NULL){
+        sp_free_port_list(ports);
+        syslog(LOG_ERR, "Device_list not initialized");
+        return ERR_NULL_POINTER;
+    }
+
+    uint32_t device_index = 0;
+
+    for (uint32_t i = 0; ports[i] != NULL; i++){
+        int vid;
+        int pid;
+
+        result = sp_get_port_usb_vid_pid(ports[i], &vid, &pid);
+
+        if (result != SP_OK)
+            continue;
+        
+
         const char *port_name = sp_get_port_name(ports[i]);
 
         if (port_name == NULL)
-        {
             continue;
+        
+        device_list[device_index].port = malloc(strlen(port_name) + 1);
+
+        if (device_list[device_index].port == NULL){
+            device_manager_free_devices(device_list, device_index);
+            sp_free_port_list(ports);
+            return ERROR;
         }
 
-        printf("Found port: %s\n", port_name);
+        strcpy(device_list[device_index].port, port_name);
+
+        device_list[device_index].vid = vid;
+        device_list[device_index].pid = pid;
+
+        device_index++;
     }
 
     sp_free_port_list(ports);
+
+    *devices = device_list;
+    *count = device_index;
+
+    return OK;
+}
+
+ESP_Error device_manager_wait_for_change(void)
+{
+    char buffer[4096];
+
+    if (inotify_fd == -1)
+        return ERROR;
+
+    while (1){
+        ssize_t length = read(inotify_fd, buffer, sizeof(buffer));
+
+        if (length == -1){
+            if (errno == EINTR)
+                return OK;
+
+            return ERROR;
+        }
+
+        size_t offset = 0;
+
+        while (offset < (size_t)length){
+            struct inotify_event *event = (struct inotify_event *)&buffer[offset];
+
+            if (event->len > 0) {
+                if (strncmp(event->name, "ttyUSB", 6) == 0 || strncmp(event->name, "ttyACM", 6) == 0)
+                    return OK;
+            }
+            offset += sizeof(struct inotify_event) + event->len;
+        }
+    }
+}
+
+ESP_Error device_manager_update_devices(void)
+{
+    return OK;
+}
+
+void device_manager_free_devices(Device *devices, uint32_t count)
+{
+    if (devices == NULL)
+        return;
+
+    for (uint32_t i = 0; i < count; i++){
+        free(devices[i].port);
+    }
+
+    free(devices);
+}
+
+ESP_Error device_manager_clear_data(void)
+{
+    if (inotify_watch != -1){
+        inotify_rm_watch(inotify_fd, inotify_watch);
+        inotify_watch = -1;
+    }
+
+    if (inotify_fd != -1){
+        close(inotify_fd);
+        inotify_fd = -1;
+    }
 
     return OK;
 }
