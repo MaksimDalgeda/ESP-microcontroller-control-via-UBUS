fb8005fefd8e8a555bfc642a27daa4ba902984a1
Maksim Dalgeda <maksim.dalgeda@gmail.com>
2026-09-18 10:22:11 +0300

    add logs using syslog

--- src/main.c
+++ src/main.c
@@ -1,13 +1,27 @@
+#include <syslog.h>
+
 #include "service.h"
 
 int main()
 {
     ESP_Error error = OK;
     
+    openlog("esp-controller", LOG_PID | LOG_CONS, LOG_DAEMON);
+    syslog(LOG_INFO, "Application started");
+
+
     error = service_find_devices();
 
-    printf("%s\n", error_to_string(error));
+    if(error != OK)
+        goto end;
 
+
+    end:
+    if(error == OK)
+        syslog(LOG_INFO, "Application stopped without error");
+    else
+        syslog(LOG_INFO, "Application stopped with error -%s (%d)", error_to_string(error), error);
+    closelog();
     return error;
 
 }
