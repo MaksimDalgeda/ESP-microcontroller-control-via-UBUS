8d31eafb2d7cb562c40cf0bb2ec49ef7d84c8cfe
Maksim Dalgeda <maksim.dalgeda@gmail.com>
2026-09-18 16:09:59 +0300

    add ubus functions prototypes

--- include/ubus.h
+++ include/ubus.h
@@ -2,22 +2,19 @@
 #define UBUS_H
 
 #include <libubus.h>
-#include <syslog.h>
 
 #include "error.h"
 
 typedef struct
 {
     struct ubus_context *ctx;
-    uint32_t system_id;
-    uint32_t lan_id;
-    uint32_t wan_id;
-    uint32_t network_device_id;
 
 } Ubus_State; 
 
 
 ESP_Error initialize_ubus(void);
+ESP_Error register_ubus_object(void);
+ESP_Error run_ubus(void);
 ESP_Error disconnect_ubus(void);
 
 
---
+++ include/ubus_methods.h
@@ -0,0 +1,10 @@
+#ifndef UBUS_METHODS_H
+#define UBUS_METHODS_H
+
+#include <libubus.h>
+
+#include "error.h"
+
+ESP_Error initialize_ubus_methods(void);
+
+#endif
