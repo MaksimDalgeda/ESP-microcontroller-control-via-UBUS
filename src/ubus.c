7904327b0b8678a313343874538a35a136d2422d
Maksim Dalgeda <maksim.dalgeda@gmail.com>
2026-09-21 16:47:23 +0300

    implment register_ubus_object fucntion

--- src/ubus.c
+++ src/ubus.c
@@ -1,13 +1,52 @@
 #include "ubus.h"
-
+#include "ubus_methods.h"
 static Ubus_State g_ubus;
 
 ESP_Error initialize_ubus(void){
 
+    if(uloop_init() != 0){
+        syslog(LOG_ERR, "UBUS service failed to initialize ubus");
+        return ERR_UBUS_INVOKE;
+    }
     g_ubus.ctx = ubus_connect(NULL);
 
-    if(g_ubus.ctx == NULL)
+    if (g_ubus.ctx == NULL){
+        uloop_done();
         return ERR_UBUS_CONNECT;
+    }
+
+    return OK;
+}
+
+ESP_Error register_ubus_object(void)
+{
+    if(g_ubus.ctx == NULL)
+        return ERR_UBUS_NOT_INITIALIZED;
+
+    struct ubus_object *object = ubus_methods_get_object();
+
+    if(object == NULL){
+        syslog(LOG_ERR, "UBUS service failed to get object");
+        return ERR_NULL_POINTER;
+    }
+
+    int result = ubus_add_object(g_ubus.ctx, object);
+  
+    if(result != 0){
+        syslog(LOG_ERR, "Ubus service failes to register ubus object: %s (%d)", ubus_strerror(result), result);
+        return ERR_UBUS_INVOKE;
+    }
+    return OK;
+}
+
+ESP_Error run_ubus(void)
+{
+    if (g_ubus.ctx == NULL)
+        return ERR_UBUS_NOT_INITIALIZED;
+
+    ubus_add_uloop(g_ubus.ctx);
+
+    uloop_run();
 
     return OK;
 }
@@ -18,6 +57,8 @@ ESP_Error disconnect_ubus(void){
         ubus_free(g_ubus.ctx);
         g_ubus.ctx = NULL;
     }
+
+    uloop_done();
 
     return OK;
 }
