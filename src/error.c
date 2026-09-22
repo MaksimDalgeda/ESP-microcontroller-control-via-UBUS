f7e592efde45a38da4f302d7871602a71fee76f7
Maksim Dalgeda <maksim.dalgeda@gmail.com>
2026-09-22 13:08:14 +0300

    add parser fot new error types

--- src/error.c
+++ src/error.c
@@ -7,8 +7,14 @@ const char *error_to_string(ESP_Error err)
         case OK:
             return "OK";
 
+        case NO_DEVICE_UPDATE:
+            return "NO_DEVICE_UPDATE";
+
         case ERROR:
             return "ERROR";
+
+        case ERR_NULL_POINTER:
+            return "ERR_NULL_POINTER";
 
         case ERR_UBUS_CONNECT:
             return "ERR_UBUS_CONNECT";
@@ -18,6 +24,12 @@ const char *error_to_string(ESP_Error err)
 
         case ERR_UBUS_NOT_INITIALIZED:
             return "ERR_UBUS_NOT_INITIALIZED";
+
+        case ERR_GET_PORT_LIST:
+            return "ERR_GET_PORT_LIST";
+
+        case PTHREAD_MUTEX_ERROR:
+            return "PTHREAD_MUTEX_ERROR";
 
         default:
             return "UNKNOWN_ERROR";
