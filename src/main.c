47234fc0a473a641246980fb6e6b7fc52ac458c9
Maksim Dalgeda <maksim.dalgeda@gmail.com>
2026-09-22 13:10:08 +0300

    add thread usage in main.c

--- src/main.c
+++ src/main.c
@@ -1,55 +1,45 @@
 #include <syslog.h>
 
 #include "device_manager_service.h"
-#include "ubus_service.h"
 #include "signal_handler.h"
+#include "threads.h"
 
 int main(void)
 {
     ESP_Error error = OK;
-    
+
     openlog("esp-controller", LOG_PID | LOG_CONS, LOG_DAEMON);
+
     syslog(LOG_INFO, "Application started");
+
     set_signal_action();
 
-
     error = device_manager_service_init();
-    if(error != OK)
-        goto end;
 
-    error = device_manager_service_find_devices();
-    if(error != OK)
-        goto end;
- 
-    error = ubus_service_init();
-    if(error != OK)
-        goto end;
-
-    error = ubus_service_start();
     if (error != OK)
         goto end;
 
-    while(!stop)
-    {
-        error = device_manager_service_wait_for_device_change();
+    error = device_manager_service_find_devices();
 
-        if (error != OK)
-            goto end;
+    if (error != OK)
+        goto end;
 
-        error = device_manager_service_update_devices();
+    error = threads_start();
 
-        if (error != OK)
-            goto end;
-    }
+    if (error != OK)
+        goto end;
 
-    end:
-    ubus_service_clear_data();
+    error = threads_join();
+
+end:
     device_manager_service_clear_data();
-    
-    if(error == OK)
+
+    if (error == OK)
         syslog(LOG_INFO, "Application stopped without error");
     else
-        syslog(LOG_ERR, "Application stopped with error -%s (%d)", error_to_string(error), error);
+        syslog(LOG_ERR,"Application stopped with error - %s (%d)", error_to_string(error),error);
+     
     closelog();
+
     return error;
 }
