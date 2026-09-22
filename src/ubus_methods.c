01e819b65582b23fbb3a377029428bcfcb5f8ad3
Maksim Dalgeda <maksim.dalgeda@gmail.com>
2026-09-22 13:11:18 +0300

    bug fix in blob_buf initialization

--- src/ubus_methods.c
+++ src/ubus_methods.c
@@ -1,59 +1,64 @@
-    #include <libubus.h>
-    #include <libubox/blobmsg_json.h>
+#include <libubus.h>
+#include <libubox/blobmsg_json.h>
 
-    #include "ubus_methods.h"
-    #include "device_manager.h"
+#include "ubus_methods.h"
+#include "device_manager.h"
 
-    static int ubus_devices(struct ubus_context *ctx, struct ubus_object *obj, struct ubus_request_data *req, const char *method, struct blob_attr *msg)
-    {   
-        Device *devices = NULL;
-        uint32_t count = 0;
+static int ubus_devices(struct ubus_context *ctx, struct ubus_object *obj, struct ubus_request_data *req, const char *method, struct blob_attr *msg)
+{   
+    Device *devices = NULL;
+    uint32_t count = 0;
 
-        ESP_Error error = device_manager_get_devices(&devices, &count);
+    ESP_Error error = device_manager_get_devices(&devices, &count);
 
-        if(error != OK)
-            return UBUS_STATUS_UNKNOWN_ERROR;
-        
-        struct blob_buf buf;
+    if (error != OK)
+        return UBUS_STATUS_UNKNOWN_ERROR;
 
-        blob_buf_init(&buf, 0);
+    struct blob_buf buf = {0};
 
-        void *array = blobmsg_open_array(&buf, NULL);
-
-        for(int i = 0; i < count; i++){
-
-            void *table = blobmsg_open_table(&buf, NULL);
-            
-            blobmsg_add_string(&buf, "port", devices[i].port);
-            blobmsg_add_u32(&buf, "vendor_id", devices[i].vid);
-            blobmsg_add_u32(&buf, "product_id", devices[i].pid);
-
-            blobmsg_close_table(&buf, table);
-        }
-
-        blobmsg_close_array(&buf, array);
-
-        ubus_send_reply(ctx, req, buf.head);
-
-        blob_buf_free(&buf);
-
+    if (blob_buf_init(&buf, 0) != 0) {
+        syslog(LOG_ERR, "Failed to initialize blob buffer");
         device_manager_free_devices(devices, count);
-
-        return UBUS_STATUS_OK;
+        return UBUS_STATUS_UNKNOWN_ERROR;
     }
 
-    static const struct ubus_method esp_methods[] = { //add new methods(off, on, get)
-        UBUS_METHOD_NOARG("devices", ubus_devices)
-    };
+    blob_buf_init(&buf, 0);
 
-    static struct ubus_object_type esp_object_type = UBUS_OBJECT_TYPE("esp-controller", esp_methods);
+    void *array = blobmsg_open_array(&buf, NULL);
+    
+    for (uint32_t i = 0; i < count; i++) {
 
-    static struct ubus_object esp_object = {
+        void *table = blobmsg_open_table(&buf, NULL);
+
+        blobmsg_add_string(&buf, "port", devices[i].port);
+        blobmsg_add_u32(&buf, "vendor_id", devices[i].vid);
+        blobmsg_add_u32(&buf, "product_id", devices[i].pid);
+
+        blobmsg_close_table(&buf, table);
+    }
+
+    blobmsg_close_array(&buf, array);
+
+    ubus_send_reply(ctx, req, buf.head);
+
+    blob_buf_free(&buf);
+
+    device_manager_free_devices(devices, count);
+
+    return UBUS_STATUS_OK;
+}
+
+//add new methods(off, on, get)
+static const struct ubus_method esp_methods[] = { UBUS_METHOD_NOARG("devices", ubus_devices)};
+
+static struct ubus_object_type esp_object_type = UBUS_OBJECT_TYPE("esp-controller", esp_methods);
+
+static struct ubus_object esp_object = {
         .name = "esp-controller",
         .type = &esp_object_type,
         .methods = esp_methods,
         .n_methods = sizeof(esp_methods) / sizeof(esp_methods[0]),
-    };
+};
 
 struct ubus_object *ubus_methods_get_object(void)
 {
