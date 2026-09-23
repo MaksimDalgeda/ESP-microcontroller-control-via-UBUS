f25aa1bb2de2641a7c6dd9b5e0355343ad10829b
Maksim Dalgeda <maksim.dalgeda@gmail.com>
2026-09-23 10:54:05 +0300

    deleted unused fucntion prototypes

--- include/esp_controller.h
+++ include/esp_controller.h
@@ -4,16 +4,10 @@
 #include "device_manager.h"
 #include "error.h"
 
-ESP_Error esp_controller_connect(Device *device);
-
-ESP_Error esp_controller_disconnect(Device *device);
-
-ESP_Error esp_controller_test_connection(Device *device);
+ESP_Error esp_controller_test_connection(Device *device);//???
 
 ESP_Error esp_controller_on(Device *device, int pin);
-
 ESP_Error esp_controller_off(Device *device, int pin);
-
 ESP_Error esp_controller_get(Device *device, int pin, const char *model, const char *sensor);
 
 #endif
