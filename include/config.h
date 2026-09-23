35dd6df7b11ded7294834ab61e1112ba9dc3d4df
Maksim Dalgeda <maksim.dalgeda@gmail.com>
2026-09-23 13:45:18 +0300

    add supported pins list

--- include/config.h
+++ include/config.h
@@ -15,4 +15,16 @@
 
 #define ESP_COMMAND_BUFFER_SIZE    64
 #define ESP_RESPONSE_BUFFER_SIZE   256
+
+//pins
+#define ESP_PIN_D0    16
+#define ESP_PIN_D1    5
+#define ESP_PIN_D2    4
+#define ESP_PIN_D3    0
+#define ESP_PIN_D4    2
+#define ESP_PIN_D5    14
+#define ESP_PIN_D6    12
+#define ESP_PIN_D7    13
+#define ESP_PIN_D8    15
+
 #endif
