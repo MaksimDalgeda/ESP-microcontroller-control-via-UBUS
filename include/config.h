fb3b7b3ff6456a6abf2df2612d1ed76c4d299424
Maksim Dalgeda <maksim.dalgeda@gmail.com>
2026-09-25 14:05:14 +0300

    delete unused pins definitions from config

--- include/config.h
+++ include/config.h
@@ -18,15 +18,4 @@
 #define ESP_RESPONSE_BUFFER_SIZE   256
 #define DEVICE_CHANGE_BUFFER_SIZE  4096
 
-//pins
-#define ESP_PIN_D0    16
-#define ESP_PIN_D1    5
-#define ESP_PIN_D2    4
-#define ESP_PIN_D3    0
-#define ESP_PIN_D4    2
-#define ESP_PIN_D5    14
-#define ESP_PIN_D6    12
-#define ESP_PIN_D7    13
-#define ESP_PIN_D8    15
-
 #endif
